ddd76bfec18cb11a2e12b8de05d294cb9a62e424
Rick Alderson <rick4lderson@gmail.com>
2026-01-15 22:55:52 +0300

    Обновил скрипт в соответствии с принципами чистого кода

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ import argparse
 from random import choice
 from string import ascii_letters, digits
 
+SPECIAL_CHARS = '!@#$%^&*()_+=-{}[]|\\;:\'",.<>/?'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--length', type=int, default=8, help='Длина пароля')
-args = parser.parse_args()
 
-alphabet = ascii_letters + digits + '!@#$%^&*()_+=-{}[]|\\;:\'",.<>/?'
-for _ in range(args.length):
-    print(choice(alphabet), end='', flush=True)
+def generate_password(length: int) -> str:
+    """Генерирует случайный пароль заданной длины."""
+    allowed_chars = ascii_letters + digits + SPECIAL_CHARS
+    return ''.join(choice(allowed_chars) for _ in range(length))
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--length', type=int, default=8, help='Длина пароля')
+    args = parser.parse_args()
+
+    if args.length <= 0:
+        print("Длина пароля должна быть положительным числом")
+        return
+
+    password = generate_password(args.length)
+    print(password)
+
+
+if __name__ == '__main__':
+    main()
